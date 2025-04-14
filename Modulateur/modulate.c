283a70799e47c6cedd2049b073efb8a74e4cf444
gwilcox019 <gwilcox019@gmail.com>
2025-04-14 17:04:22 +0200

    neon mod + demod

--- Modulateur/modulate.c
+++ Modulateur/modulate.c
@@ -5,6 +5,16 @@ void module_bpsk_modulate (const uint8_t* CN, int32_t* XN, size_t n) {
         XN[n-1] = (CN[n-1]?-1:1);
 }
 
+void module_bpsk_modulate_neon (const uint8_t* CN, int32_t* XN, size_t n) {
+    int8x16_t l8; // temp vector for loading array
+    int8x16_t one = vdupq_n_s8(1); // bit mask 
+    for (int i=0; i<n; i+=16) {
+        l8 = vld1q_s8((int8_t*)CN+i); // load next 16 values
+        l8 = (int8x16_t)vcltzq_s8(l8); // compare < 0
+        vst1q_s8((int8_t*)XN+i,vandq_s8(l8,one)); // mask to get 1 or 0, store in XN
+    }
+}
+
 void modem_BPSK_modulate_all_ones(const uint8_t *C_N, int32_t *X_N, size_t N) {
     for (; N>0; N--)
         X_N[N-1] = 1;
@@ -16,6 +26,17 @@ void modem_BPSK_demodulate (const float* Y_N, float* L_N, size_t n, float sigma)
         L_N[i] = Y_N[i] * constant; 
     }
 }
+
+void modem_BPSK_demodulate_neon (const float* Y_N, float* L_N, size_t n, float sigma) {
+    float32x4_t tmp, cst;
+    float constant = 2.0f/(sigma*sigma);
+    cst = vdupq_n_f32(constant); 
+    for (int i=0; i<n; i+=4) {
+        tmp = vld1q_f32(Y_N+i); 
+        vst1q_f32(L_N+i,vmulq_f32(cst,tmp));
+    }
+}
+
 
 // adds random noise following a normal distribution
 void channel_AGWN_add_noise(const int32_t* X_N, float* Y_N, size_t n, float sigma, gsl_rng * rangen) {
--- Modulateur/modulate.h
+++ Modulateur/modulate.h
@@ -5,16 +5,23 @@
 #include <stdio.h>
 #include <gsl/gsl_rng.h>
 #include <gsl/gsl_randist.h>
+#include <arm_neon.h>
 
 // modulates encoded codeword where 0 -> 1, 1 -> -1
 // read from C_N, write into X_N
 void module_bpsk_modulate (const uint8_t* CN, int32_t* XN, size_t n);
+
+// using SIMD
+void module_bpsk_modulate_neon (const uint8_t* CN, int32_t* XN, size_t n);
 
 // Always outputs 1s
 void modem_BPSK_modulate_all_ones(const uint8_t *C_N, int32_t *X_N, size_t N);
 
 //Multiplies by a factor 2/sigma² to ignore sigma values
 void modem_BPSK_demodulate (const float* Y_N, float* L_N, size_t n, float sigma);
+
+// using SIMD
+void modem_BPSK_demodulate_neon (const float* Y_N, float* L_N, size_t n, float sigma);
 
 //Adds noise to value
 void channel_AGWN_add_noise(const int32_t* X_N, float* Y_N, size_t n, float sigma, gsl_rng * rangen);
