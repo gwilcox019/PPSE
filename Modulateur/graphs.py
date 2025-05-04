85a53fa47c235a48fe656db67d20a34038cdc189
gwilcox019 <gwilcox019@gmail.com>
2025-05-04 19:46:16 +0200

    pretty graphs

--- Modulateur/graphs.py
+++ Modulateur/graphs.py
@@ -8,33 +8,44 @@ import pandas
 
 ## Files = (namefile, legend, print format)
 ## with print format being first the dot style (x, +, . ...) and then the line style (usually - for continuous or -- for dashed)
-types = ["hard", "soft_32", "soft_64", "soft_96", "soft_128"]
+#types = ["hard", "soft_32", "soft_64", "soft_96", "soft_128"]
 
 files = [ 
-         ("normal/sim_", "Normal - K = 32", "yx--"),
+         #("normal/sim_", "Normal - K = 32", "yx--"),
          #("normal/big_k/sim_", "Normal - K = 128", "gx--"),
-         ("neon_mod/sim_", "Neon modulator - K = 32", "r.-"),
+         #("neon_mod/sim_", "Neon modulator - K = 32", "r.-"),
          #("neon_demod/big_k/sim_", "Neon demodulator - K = 128", "b.-"), 
+         ("sim_pack_stats.csv", "Bit packing", ".-"),
+         ("sim_no-pack_stats.csv", "Normal", "x-")
 ]
 
-for t in types:
-    output = "FINAL GRAPHS/neon_modul/block throughput/"+t
-    xlabel = "Signal to Noise Ratio (Eb/N0) (dB)"  
-    ylabel = "Demodulator throughput (Mbps)"
-    x = "Eb/No"
-    y = "demodulate_thr"
+#for t in types:
+    #output = "FINAL GRAPHS/neon_modul/block throughput/"+t
+xlabel = "Signal to Noise Ratio (Eb/N0) (dB)"  
+ylabel = "Average block time"
+    #"Demodulator throughput (Mbps)"
+x = "Eb/No"
+y1 = "gen_avg"
+y2 = "encode_avg"
+y3 = "bpsk_avg"
+    #"demodulate_thr"
 
-    plt.figure()
-    for elem in files:
-        sim = pandas.read_csv(elem[0]+t+"_stats.csv")
+plt.figure()
+for elem in files:
+        #sim = pandas.read_csv(elem[0]+t+"_stats.csv")
+        sim = pandas.read_csv(elem[0])
         simX = sim[[x]]
-        simY = sim[[y]]
-        plt.plot(simX, simY, elem[2], label=elem[1])
+        simY = sim[[y1]]
+        plt.plot(simX, simY, elem[2]+"r", label="Generator "+elem[1])
+        simY = sim[[y2]]
+        plt.plot(simX, simY, elem[2]+"g", label="Encoder "+elem[1])
+        simY = sim[[y3]]
+        plt.plot(simX, simY, elem[2]+"b", label="Modulator "+elem[1])
 
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
-    plt.yscale("log")
-    plt.legend()
-    plt.grid()
-    plt.savefig(output+".jpg", format="jpg")
+plt.xlabel(xlabel)
+plt.ylabel(ylabel)
+    #plt.yscale("log")
+plt.legend()
+plt.grid()
+plt.savefig("bitpack_time.jpg", format="jpg")
     
